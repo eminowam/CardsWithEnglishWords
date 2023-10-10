33ea9a19166c9f303f2603b535548ae31dc544c1
aimeerimeminova <aimemynoo@gmail.com>
2023-10-10 17:54:47 +0600

    AddWord is done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,6 +75,11 @@ dependencies {
     implementation ("androidx.room:room-ktx:2.5.2")
     kapt ("androidx.room:room-compiler:2.5.2")
 
+    //Coroutines
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
+    implementation ("androidx.room:room-ktx:2.5.2")
+
+
 
 
 
--- app/src/main/java/com/example/cardswithenglishwords/presentation/fragments/ListFragment.kt
+++ app/src/main/java/com/example/cardswithenglishwords/presentation/fragments/ListFragment.kt
@@ -1,18 +1,23 @@
 package com.example.cardswithenglishwords.presentation.fragments
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.lifecycleScope
+import com.example.cardswithenglishwords.data.storage.WordDao
+import com.example.cardswithenglishwords.data.storage.WordDataBase
 import com.example.cardswithenglishwords.databinding.FragmentListBinding
 import com.example.cardswithenglishwords.presentation.adapter.WordsAdapter
-import com.example.cardswithenglishwords.presentation.viewmodel.HomeScreenViewModel
 import com.example.cardswithenglishwords.presentation.viewmodel.ListViewModel
+import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class ListFragment : Fragment() {
+abstract class ListFragment(
+    private var wordsDao: WordDao
+) : Fragment() {
 
     private val binding by lazy {
         FragmentListBinding.inflate(layoutInflater)
@@ -32,11 +37,16 @@ class ListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         observeRv()
+        val db = WordDataBase.getInstance(this.requireContext())
+//        db.wordDao().getAllWords().asLiveData().observe(viewLifecycleOwner) { list ->
+        wordsDao = db.wordDao()
         observeWords()
+
     }
 
     private fun observeRv() = with(binding) {
         recyclerViewStorage.adapter = listWordsAdapter
+
     }
 
     private fun observeWords() = with(viewModel) {
